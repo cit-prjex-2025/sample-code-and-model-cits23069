c7a96f534b86f4897e7fe34bb002c7c9e78cbd0a
cits23069 <cits23069@g.nihon-u.ac.jp>
2025-10-21 16:53:28 +0900

    しゃしん

--- codes/sample04/app.c
+++ codes/sample04/app.c
@@ -1,22 +1,24 @@
 #include "app.h"
 #include "util.h"
+#include "timer.h" // タイマーのヘッダをインクルード
+#include "horn.h"  // ホーンのヘッダをインクルード
 
 const int bumper_sensor = EV3_PORT_1;
 const int linemon_sensor = EV3_PORT_3;
 const int left_motor = EV3_PORT_A;
 const int right_motor = EV3_PORT_C;
 
-const int carrier_sensor = EV3_PORT_2; // <1>
+const int carrier_sensor = EV3_PORT_2;
 
-int carrier_cargo_is_loaded(void) { // <2>
+int carrier_cargo_is_loaded(void) {
   return ev3_touch_sensor_is_pressed(carrier_sensor);
 }
 
-const int walldetector_sensor = EV3_PORT_4; // <3>
-#define WD_DISTANCE 10 // <4>
-int wd_distance = WD_DISTANCE; // <5>
+const int walldetector_sensor = EV3_PORT_4;
+#define WD_DISTANCE 10
+int wd_distance = WD_DISTANCE;
 
-int wall_detector_is_detected(void) { // <6>
+int wall_detector_is_detected(void) {
   return ev3_ultrasonic_sensor_get_distance(walldetector_sensor)
     < wd_distance;
 }
@@ -62,28 +64,42 @@ void tracer_stop(void) {
   driver_stop();
 }
 
-typedef enum { // <1>
+typedef enum {
   P_WAIT_FOR_LOADING, P_TRANSPORTING,
   P_WAIT_FOR_UNLOADING, P_RETURNING, P_ARRIVED
-} porter_state; // <2>
+} porter_state;
 
-porter_state p_state = P_WAIT_FOR_LOADING; // <3>
+porter_state p_state = P_WAIT_FOR_LOADING;
 
-int p_entry = true; // <4>
+int p_entry = true;
+
+// 荷物の積載・荷降ろし後の待機時間 (ミリ秒)
+#define CARGO_WAIT_TIME 2000
 
 void porter_transport(void) {
-  num_f(p_state, 2); // <1>
+  num_f(p_state, 2);
   switch(p_state) {
-  case P_WAIT_FOR_LOADING: // <2>
-    if( p_entry ) { // <3>
+  case P_WAIT_FOR_LOADING:
+    if( p_entry ) {
       p_entry = false;
     }
-    // <4>
-    if( carrier_cargo_is_loaded() ) { // <5>
-      p_state = P_TRANSPORTING; // <6>
-      p_entry = true; // <7>
+    // 荷物が積まれ、タイマーが動いていない場合
+    if( carrier_cargo_is_loaded() && !timer_is_started() ) {
+        horn_confirmation(); // 確認音を鳴らす
+        timer_start(CARGO_WAIT_TIME); // 2秒タイマーを開始
     }
-    if( p_entry ) { // <8>
+    // タイマー中に荷物が降ろされた場合
+    else if ( !carrier_cargo_is_loaded() && timer_is_started() ) {
+        timer_stop(); // タイマーをキャンセル
+    }
+    // 荷物が積まれた状態でタイムアウトした場合
+    else if ( carrier_cargo_is_loaded() && timer_is_timedout() ) {
+        timer_stop(); // タイマーを停止
+        p_state = P_TRANSPORTING;
+        p_entry = true;
+    }
+
+    if( p_entry ) {
       // exit
     }
     break;
@@ -93,6 +109,7 @@ void porter_transport(void) {
     }
     tracer_run();
     if( wall_detector_is_detected() ) {
+      horn_arrived(); // 到着音を鳴らす
       p_state = P_WAIT_FOR_UNLOADING;
       p_entry = true;
     }
@@ -104,11 +121,20 @@ void porter_transport(void) {
     if( p_entry ) {
       p_entry = false;
     }
-    if(! carrier_cargo_is_loaded() ) {
-      p_state = P_RETURNING;
-      p_entry = true;
+    // 荷物が降ろされ、タイマーが動いていない場合
+    if( !carrier_cargo_is_loaded() && !timer_is_started() ) {
+        timer_start(CARGO_WAIT_TIME); // 2秒タイマーを開始
     }
-    // do
+    // タイマー中に荷物が再度積まれた場合
+    else if ( carrier_cargo_is_loaded() && timer_is_started() ) {
+        timer_stop(); // タイマーをキャンセル
+    }
+    // 荷物が降ろされた状態でタイムアウトした場合
+    else if ( !carrier_cargo_is_loaded() && timer_is_timedout() ) {
+        timer_stop(); // タイマーを停止
+        p_state = P_RETURNING;
+        p_entry = true;
+    }
     if( p_entry ) {
       // exit
     }
@@ -119,6 +145,7 @@ void porter_transport(void) {
     }
     tracer_run();
     if( bumper_is_pushed() ) {
+      horn_arrived(); // 到着音を鳴らす
       p_state = P_ARRIVED;
       p_entry = true;
     }
@@ -127,6 +154,7 @@ void porter_transport(void) {
     }
     break;
   case P_ARRIVED:
+    // 何もしない
     break;
   default:
     break;
@@ -137,15 +165,169 @@ void main_task(intptr_t unused) {
   static int is_initialized = false;
   if(! is_initialized ) {
     is_initialized = true;
-    init_f("sample04");
+    init_f("sample04_with_th");
     ev3_motor_config(left_motor, LARGE_MOTOR);
     ev3_motor_config(right_motor, LARGE_MOTOR);
-    ev3_sensor_config(walldetector_sensor, ULTRASONIC_SENSOR); // <1>
+    ev3_sensor_config(walldetector_sensor, ULTRASONIC_SENSOR);
     ev3_sensor_config(linemon_sensor, COLOR_SENSOR);
     ev3_sensor_config(bumper_sensor, TOUCH_SENSOR);
-    ev3_sensor_config(carrier_sensor, TOUCH_SENSOR); // <2>
+    ev3_sensor_config(carrier_sensor, TOUCH_SENSOR);
   }
 
   porter_transport();
   ext_tsk();
 }
+
+
+
+// #include "app.h"
+// #include "util.h"
+
+// const int bumper_sensor = EV3_PORT_1;
+// const int linemon_sensor = EV3_PORT_3;
+// const int left_motor = EV3_PORT_A;
+// const int right_motor = EV3_PORT_C;
+
+// const int carrier_sensor = EV3_PORT_2; // <1>
+
+// int carrier_cargo_is_loaded(void) { // <2>
+//   return ev3_touch_sensor_is_pressed(carrier_sensor);
+// }
+
+// const int walldetector_sensor = EV3_PORT_4; // <3>
+// #define WD_DISTANCE 10 // <4>
+// int wd_distance = WD_DISTANCE; // <5>
+
+// int wall_detector_is_detected(void) { // <6>
+//   return ev3_ultrasonic_sensor_get_distance(walldetector_sensor)
+//     < wd_distance;
+// }
+
+// int bumper_is_pushed(void) {
+//   return ev3_touch_sensor_is_pressed(bumper_sensor);
+// }
+
+// #define LM_THRESHOLD 20
+// int lm_threshold = LM_THRESHOLD;
+
+// int linemon_is_online(void) {
+//   return ev3_color_sensor_get_reflect(linemon_sensor) < lm_threshold;
+// }
+
+// #define DR_POWER 20
+// int dr_power = DR_POWER;
+
+// void driver_turn_left(void) {
+//   ev3_motor_set_power(EV3_PORT_A, 0);
+//   ev3_motor_set_power(EV3_PORT_C, dr_power);
+// }
+
+// void driver_turn_right(void) {
+//   ev3_motor_set_power(EV3_PORT_A, dr_power);
+//   ev3_motor_set_power(EV3_PORT_C, 0);
+// }
+
+// void driver_stop(void) {
+//   ev3_motor_stop(left_motor, false);
+//   ev3_motor_stop(right_motor, false);
+// }
+
+// void tracer_run(void) {
+//   if( linemon_is_online() ) {
+//     driver_turn_left();
+//   } else {
+//     driver_turn_right();
+//   }
+// }
+
+// void tracer_stop(void) {
+//   driver_stop();
+// }
+
+// typedef enum { // <1>
+//   P_WAIT_FOR_LOADING, P_TRANSPORTING,
+//   P_WAIT_FOR_UNLOADING, P_RETURNING, P_ARRIVED
+// } porter_state; // <2>
+
+// porter_state p_state = P_WAIT_FOR_LOADING; // <3>
+
+// int p_entry = true; // <4>
+
+// void porter_transport(void) {
+//   num_f(p_state, 2); // <1>
+//   switch(p_state) {
+//   case P_WAIT_FOR_LOADING: // <2>
+//     if( p_entry ) { // <3>
+//       p_entry = false;
+//     }
+//     // <4>
+//     if( carrier_cargo_is_loaded() ) { // <5>
+//       p_state = P_TRANSPORTING; // <6>
+//       p_entry = true; // <7>
+//     }
+//     if( p_entry ) { // <8>
+//       // exit
+//     }
+//     break;
+//   case P_TRANSPORTING:
+//     if( p_entry ) {
+//       p_entry = false;
+//     }
+//     tracer_run();
+//     if( wall_detector_is_detected() ) {
+//       p_state = P_WAIT_FOR_UNLOADING;
+//       p_entry = true;
+//     }
+//     if( p_entry ) {
+//       tracer_stop();
+//     }
+//     break;
+//   case P_WAIT_FOR_UNLOADING:
+//     if( p_entry ) {
+//       p_entry = false;
+//     }
+//     if(! carrier_cargo_is_loaded() ) {
+//       p_state = P_RETURNING;
+//       p_entry = true;
+//     }
+//     // do
+//     if( p_entry ) {
+//       // exit
+//     }
+//     break;
+//   case P_RETURNING:
+//     if( p_entry ) {
+//       p_entry = false;
+//     }
+//     tracer_run();
+//     if( bumper_is_pushed() ) {
+//       p_state = P_ARRIVED;
+//       p_entry = true;
+//     }
+//     if( p_entry ) {
+//       tracer_stop();
+//     }
+//     break;
+//   case P_ARRIVED:
+//     break;
+//   default:
+//     break;
+//   }
+// }
+
+// void main_task(intptr_t unused) {
+//   static int is_initialized = false;
+//   if(! is_initialized ) {
+//     is_initialized = true;
+//     init_f("sample04");
+//     ev3_motor_config(left_motor, LARGE_MOTOR);
+//     ev3_motor_config(right_motor, LARGE_MOTOR);
+//     ev3_sensor_config(walldetector_sensor, ULTRASONIC_SENSOR); // <1>
+//     ev3_sensor_config(linemon_sensor, COLOR_SENSOR);
+//     ev3_sensor_config(bumper_sensor, TOUCH_SENSOR);
+//     ev3_sensor_config(carrier_sensor, TOUCH_SENSOR); // <2>
+//   }
+
+//   porter_transport();
+//   ext_tsk();
+// }
